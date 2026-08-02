b06c3d1f1e5752c0bbf2977fd905268074d92df4
Divyanshu Sharma <divyanshurprcg@gmail.com>
2026-08-02 07:43:15 +0530

    Solved LeetCode : Count Valid Prefixes (with AI Analysis)

---
+++ LeetCode/Uncategorized/Easy/count-valid-prefixes/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countValidPrefixes(string s) {
+        int i=0;
+        int zeros = 0;
+        int ones = 0;
+        int count = 0;
+
+        while(i < s.length())
+        {
+            if(s[i] == '0')
+                zeros++;
+            else
+                ones++;
+            if(zeros - ones <= 1)
+                count++;
+        }
+        return count;
+    }
+};
